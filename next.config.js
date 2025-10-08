43ccc78e9e7d34a86f393d576f40bc5afa2f9ecc
yanruoyi999 <yanruoyi999@gmail.com>
2025-10-08 11:56:15 +0800

    chore: loosen csp for google analytics

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "img-src 'self' data: https://res.cloudinary.com https://via.placeholder.com",
-      "script-src 'self' 'unsafe-inline'",
+      "img-src 'self' data: https://res.cloudinary.com https://via.placeholder.com https://www.google-analytics.com https://www.googletagmanager.com",
+      "script-src 'self' 'unsafe-inline' https://www.googletagmanager.com",
       "style-src 'self' 'unsafe-inline'",
-      "connect-src 'self' https://res.cloudinary.com",
+      "connect-src 'self' https://res.cloudinary.com https://www.google-analytics.com https://www.googletagmanager.com",
       "frame-src 'self' https://*.github.io https://sudoku.tn1ck.com http://patorjk.com https://playpager.com https://cloud.onlinegames.io",
       "frame-ancestors 'none'",
     ].join('; '),
